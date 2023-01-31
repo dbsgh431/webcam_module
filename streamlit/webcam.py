7c30840da984e120c9ee756b97459745310f0952
yoonho laptop <dbsgh431@jbnu.ac.kr>
2023-01-31 10:22:08 +0900

    위도, 경도 추가 post 완성

--- streamlit/webcam.py
+++ streamlit/webcam.py
@@ -1,9 +1,10 @@
 import cv2
 import streamlit as st
 import requests
-from PIL import Image
-import time,io
-from datetime import datetime
+import time,io, threading
+import schedule
+import requests, json
+
 
 st.set_page_config(layout="wide")
 
@@ -11,24 +12,52 @@ st.title("Webcam Live Feed")
 run = st.checkbox('실행')
 FRAME_WINDOW = st.image([])
 
+numbers = st.empty()
 
+def current_location():
+    here_req = requests.get("http://www.geoplugin.net/json.gp")
+
+    if (here_req.status_code != 200):
+        print("현재좌표를 불러올 수 없음")
+    else:
+        location = json.loads(here_req.text)
+        crd = {"lat": str(location["geoplugin_latitude"]), "lng": str(location["geoplugin_longitude"])}
+ 
+    return crd
+
+    
+def inference(files, loc):
+
+    response = requests.post('http://118.67.129.236:30011/OD', data=loc, files=files)
+    #response = requests.post('http://118.67.129.236:30011/OD', files=files, data=crd)
+    label = response     
+    with numbers.container():
+        st.write(f'label is {label}')
+
+
+delta = 0
+previous = time.time()
 while run:
-    camera = cv2.VideoCapture('http://192.168.10.103:8080/video')
+    camera = cv2.VideoCapture('http://192.168.0.3:8080/video')
     _, frame = camera.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
-
     is_success, im_buf_arr = cv2.imencode(".jpg", frame)
-    io_buf = io.BytesIO(im_buf_arr)
-    byte_im = io_buf.getvalue()
-    files = [
-    ('files', (byte_im))
-    ]
-    print(f"time: {datetime.now()}")
-    time.sleep(3)
-    response = requests.post("http://49.50.175.25:30001/predict", files=files)
-    label = response.json()["result"]
-    st.write(f'label is {label}')
+    current = time.time()
+    delta += current - previous
+    previous = current
+
+    if delta > 3.0:
+        io_buf = io.BytesIO(im_buf_arr)
+        byte_im = io_buf.getvalue()
+        crd = current_location()
+        loc = {"lat" : crd['lat'], "lng":crd['lng']}
+        # crd = {'lat': crd['lat'], 'lng': crd['lng']}
+
+        files = [{'files', (byte_im)}]
+        print(crd)
+        inference(files, loc)
+        delta = 0
 else:
     st.write('Stopped')
 
